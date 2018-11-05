9220a8c5f0807b7918dde3ddb258581ddcc7aacd
Gayllaume <claquin@lacroixrouge-brest.fr>
2018-11-05 16:03:25 +0100

    Creation de l'ihm du robot : 4 boutons et 2 input texte pour 2 moteur

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mon 120eme sites Web</title>
+</head>
+<body style="text-align: center;">
+	<dir id="titre" style="border-style: solid;"><h1>Mon site Web</h1></dir>
+	<div style="border-style: solid;">
+		<button>-</button
+	</div>
+	<div id="php">
+		<?php
+			echo  "Nous sommes aujourd'hui " . date("l").".";
+			phpinfo();
+		?>
+	</div>
+	
+</html>
